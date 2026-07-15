ffd9c7bd3894785655290f2949602e39a5fdba6f
MaudDK <62085173+MaudDK@users.noreply.github.com>
2026-07-15 03:40:38 +0100

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        std::unordered_set<int> s;
+        for (auto num: nums){
+            if (s.contains(num)){
+                return true;
+            }
+            s.insert(num);
+        }
+        return false;
+        
+    }
+};

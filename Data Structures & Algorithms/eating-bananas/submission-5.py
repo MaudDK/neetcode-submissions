62c13dc9a10b1aee01664d1afc3524b2d70e4e50
MaudDK <62085173+MaudDK@users.noreply.github.com>
2026-09-09 01:43:03 -0700

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l = 1
+        r = max(piles) #O(N)
+        mini = r
+
+        while l <= r: #O(Log(R))
+            k = l + (r - l) // 2
+
+            #O(N) Hour Simulation Count
+            hours = 0
+            for bananas in piles:
+                hours += self.eatBananas(bananas, k)
+
+            if hours <= h: #Decrease banan eating rate
+                mini = k
+                r = k - 1
+            else: #Increase bana eating rate
+                l = k + 1
+
+        return mini
+
+    def eatBananas(self, bananas, eatRate):
+        hours = math.ceil(bananas / eatRate)
+        return hours
+
+
+        
